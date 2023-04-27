35a68d904dda3abe9a834b41c5cabe7a66b830f2
도경 <125863754+rudeh2926@users.noreply.github.com>
2023-04-27 14:15:21 +0900

    [Bronze I] Title: 더하기 사이클, Time: 212 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		in.close();
+		int cnt = 0, copy = N;
+		while (true) {
+			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+			cnt++;
+			if (copy == N) break;
+		}
+		System.out.println(cnt);
+	}
+}
