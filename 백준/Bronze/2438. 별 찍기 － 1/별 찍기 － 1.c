e9c8625f5cf0852915087fb9c5d86997a86b018d
도경 <125863754+rudeh2926@users.noreply.github.com>
2023-05-19 19:50:51 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 int main() {
-	int n;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
+	int a;
+	scanf("%d", &a);
+	for (int i = 0; i < a; i++) {
 		for (int j = 0; j <= i; j++) {
 			printf("*");
 		}
